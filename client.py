a29c876452c72b36fbdf358cec9e7fcbb4eeddad
Elsa DSJ <exaltia@exaltia.org>
2018-03-11 19:49:33 +0100

    non working reordering tentative

--- client.py
+++ client.py
@@ -3,55 +3,88 @@ import socket
 import sys
 import os
 import _thread
-from time import sleep
+from time import sleep, time
 import struct
 import fcntl
 import select
+import random
+import binascii
 def sender():
     print('sender ready')
+    print('with ipv4 packer check')
     while True:
         try:
+
             s2.sendto(out_queue.pop(0), (clientconfig['remotehost3'], int(clientconfig['remoteport3'])))
-            s1.sendto(out_queue.pop(0), (clientconfig['remotehost2'], int(clientconfig['remoteport2'])))
+            # sleep(0.01)
             s.sendto(out_queue.pop(0), (clientconfig['remotehost1'], int(clientconfig['remoteport1'])))
+            s1.sendto(out_queue.pop(0), (clientconfig['remotehost2'], int(clientconfig['remoteport2'])))
+            # sleep(0.001)
+        except ValueError:
             sleep(0.001)
         except IndexError:
-            sleep(0.0001)
+            sleep(0.001)
         except KeyboardInterrupt:
             sys.exit(0)
         except:
+            print('problem in sender')
             print(sys.exc_info())
 def taphandling():
-    next_one_out = 0
-    next_one_in = 0
+    # next_one_out = 0
+    # next_one_in = 0
+    next_needed = []
     inputs = [tap]
     outputs = [tap]
+    previous_sequence = 1 #not zero because zero is used by tcp for starting the sequence numbers
     print('tap handling ready!')
     while True:
-        if next_one_out >= 255:
-            next_one_out = 0
-        if next_one_in >= 255:
-            next_one_in = 0
+        # if next_one_out >= 255:
+        #     next_one_out = 0
+        # if next_one_in >= 255:
+        #     next_one_in = 0
         try:
             readable, writable, exceptional = select.select(inputs, outputs, inputs)
             if readable:
-                out_queue.append(bytes(str(next_one_out) + '&', 'ascii') + tap.read(1500))
-                next_one_out += 1
+                pktBytes = tap.read(1500)
+                directihlbytes= pktBytes[0:1]
+                directihlint = int.from_bytes(directihlbytes, 'big')
+                directihlbin = '{0:0{1}b}'.format(directihlint,8)
+                realdirectihl = int(directihlbin[4:8], 2) * 4
+                tcpheader = pktBytes[realdirectihl:realdirectihl+20]
+                #Tcp header start is variable due to possible options and padding in ip header, but what is of interest here, the sequence number, is always at the same position in the tcp header
+                tcpheaderInt = int.from_bytes(tcpheader, 'big')
+                tcpheaderBin = '{0:0{1}b}'.format(tcpheaderInt,20*8)
+                # next_needed.append(int(tcpheaderBin[64:128], 2))
+                sequence = (int(tcpheaderBin[32:64], 2))
+                out_queue.append(bytes(str(sequence) + '&', 'ascii') + pktBytes)
             else:
                 try:
-                    print('next one in is', min(orderer_dict), 'next one out is', next_one_out, end='\r' )
-                    tap.write(orderer_dict.pop(min(orderer_dict)))
+                    # print('je write')
+                    # print('next one in is', min(orderer_dict), 'next one out is', next_one_out, end='\r' )
+                    # print('Dict len', len(orderer_dict), end='\r')
+                    # next_pkt =orderer_dict.pop(min(orderer_dict))
+                    # print(next_pkt)
+                    next_pkt = in_queue.pop(in_queue.index(min(in_queue)))
+                    next_pkt = next_pkt.split(b'&', 1)
+                    tap.write(next_pkt[1])
+                    # sleep(0.01)
+                    # tap.write(in_queue.pop(in_queue.index(random.choice(in_queue))))
+                # except struct.error:
+                #     raise ValueError()
+                    #
+                    # sleep(0.001)
+                    # print('struct error')
                 except KeyError:
-                    sleep(0.0001)
-                except ValueError:
                     sleep(0.001)
                 except ValueError:
+                    # out_queue.append(bytes(str(time()) + '&', 'ascii'))
                     sleep(0.001)
+                except ValueError:
+                    sleep(0.05)
                 except IndexError:
-                    sleep(0.0001)
+                    sleep(0.001)
                 except:
                     print(sys.exc_info())
-
         except KeyboardInterrupt:
             sys.exit(0)
         except:
@@ -116,11 +149,15 @@ if __name__ == "__main__":
             if readable:
                 for each in readable:
                     try:
-                        preprocess = each.recv(1500)
-                        preprocess = preprocess.split(b'&', 1)
-                        pos_zero = preprocess[0]
-                        preprocess[0] = pos_zero.decode()
-                        orderer_dict[preprocess[0]] = preprocess[1]
+                        # preprocess = each.recv(1500)
+                        # print('preprocess', preprocess)
+                        # preprocess = preprocess.split(b'&', 1)
+                        # pos_zero = preprocess[0]
+                        # # preprocess[0] = float(pos_zero.decode())
+                        # orderer_dict[preprocess[0]] = preprocess[1]
+                        # print('newest dict entry', orderer_dict[preprocess[0]])
+                        in_queue.append(each.recv(1500))#[preprocess[0], preprocess[1]]
+                        # print('received')
                     except UnicodeDecodeError:
                         sleep(0.0001)
             sleep(0.0001)
--- server.py
+++ server.py
@@ -1,19 +1,30 @@
 import socket
 import sys
 import os
-from time import sleep
+from time import sleep, time
 import _thread
 import struct
 import fcntl
 import select
+import random
+import binascii
 def sender():
     while True:
+
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
         try:
+            # print('out_queue len', len(out_queue), end='\r')
+            # print('out queue', out_queue)
             s2.sendto(out_queue.pop(0), (otherend[2][0], otherend[2][1]))
-            s1.sendto(out_queue.pop(0), (otherend[1][0], otherend[1][1]))
+            #Was : s2.sendto(out_queue.pop(out_queue.index(min(out_queue))), (otherend[2][0], otherend[2][1]))
+            # sleep(0.03)
             s.sendto(out_queue.pop(0), (otherend[0][0], otherend[0][1]))
+            s1.sendto(out_queue.pop(0), (otherend[1][0], otherend[1][1]))
+            # sleep(1)
+        except ValueError:
+            sleep(0.001)
         except IndexError:
-            sleep(0.0001)
+            sleep(0.001)
         except KeyboardInterrupt:
             sys.exit(0)
         except:
@@ -21,39 +32,72 @@ def sender():
             print(sys.exc_info())
     sleep(0.001)
 def taphandling():
-    next_one_out = 0
-    next_one_in = 0
+    next_needed = []
+    # next_one_out = 0
+    # next_one_in = 0
     inputs = [tap]
     outputs = [tap]
+    previous_sequence = 1 #not zero because zero is used by tcp for starting the sequence numbers
     print('tap handling ready!')
     while True:
-        if next_one_out >= 255:
-            next_one_out = 0
-        if next_one_in >= 255:
-            next_one_in = 0
+        # if next_one_out >= 255:
+        #     next_one_out = 0
+        # if next_one_in >= 255:
+        #     next_one_in = 0
         try:
             readable, writable, exceptional = select.select(inputs, outputs, inputs)
             if readable:
-                out_queue.append(bytes(str(next_one_out) + '&', 'ascii') + tap.read(1500))
-                next_one_out += 1
+                pktBytes = tap.read(1500)
+                directihlbytes= pktBytes[0:1]
+                directihlint = int.from_bytes(directihlbytes, 'big')
+                directihlbin = '{0:0{1}b}'.format(directihlint,8)
+                realdirectihl = int(directihlbin[4:8], 2) * 4
+                tcpheader = pktBytes[realdirectihl:realdirectihl+20]
+                #Tcp header start is variable due to possible options and padding in ip header, but what is of interest here, the sequence number, is always at the same position in the tcp header
+                tcpheaderInt = int.from_bytes(tcpheader, 'big')
+                tcpheaderBin = '{0:0{1}b}'.format(tcpheaderInt,20*8)
+                # next_needed.append(int(tcpheaderBin[64:128], 2))
+                sequence = int(tcpheaderBin[32:64], 2)
+                out_queue.append(bytes(str(sequence) + '&', 'ascii') + pktBytes)
             else:
                 try:
-                    tap.write(orderer_dict.pop(min(orderer_dict)))
+                    # pass
+                    # tap.write(in_queue.pop(in_queue.index(random.choice(in_queue))))
+                    # print('Dict len', len(orderer_dict), end='\r')
+                    # print('why writable?')
+                    next_pkt = in_queue.pop(in_queue.index(min(in_queue)))
+                    next_pkt = next_pkt.split(b'&', 1)
+                    sequence = int(tcpheaderBin[64:128], 2)
+                    tap.write(next_pkt[1])
+                    # sleep(0.01)
+                    # print('next_pkt written', next_pkt)
                 except KeyError:
-                    sleep(0.0001)
-                except ValueError:
+                    print('key error')
                     sleep(0.001)
                 except ValueError:
-                    sleep(0.001)
+                    # print('value error here1')
+                    sleep(0.05)
                 except IndexError:
-                    sleep(0.0001)
+                    sleep(0.001)
                 except:
+                    print('else error?')
                     print(sys.exc_info())
+        # except struct.error:
+        #     out_queue.append(bytes(str(time()) + '&', 'ascii'))
+        #     sleep(0.001)
         except KeyboardInterrupt:
             sys.exit(0)
         except:
             print('error from taphandling')
             print(sys.exc_info())
+def liststate():
+    allseq = []
+    while True:
+        for row in in_queue:
+            lineresult = row.split(b'&',1)
+            allseq.append(lineresult[0])
+        print(allseq)
+        sleep(5)
 def starting():
     print('Initializing')
     print(rawotherend)
@@ -114,10 +158,11 @@ if __name__ == "__main__":
     print('Init OK')
     mysender = _thread.start_new_thread(sender, ())
     mytaphandler = _thread.start_new_thread(taphandling, ())
+    mystateresult = _thread.start_new_thread(liststate, ())
     print(s, s1, s2)
     nextsocket = 1
     try:
-        inputs = [s, s1, s2]
+        inputs = [s2, s1, s]
         outputs = []
         out_queue = []
         in_queue = []
@@ -126,11 +171,14 @@ if __name__ == "__main__":
             if readable:
                 for each in readable:
                     try:
-                        preprocess = each.recv(1500)
-                        preprocess = preprocess.split(b'&', 1)
-                        pos_zero = preprocess[0]
-                        preprocess[0] = pos_zero.decode()
-                        orderer_dict[preprocess[0]] = preprocess[1]
+                        # preprocess = each.recv(1500)
+                        # print('preprocess', preprocess)
+                        # preprocess = preprocess.split(b'&', 1)
+                        # pos_zero = preprocess[0]
+                        # preprocess[0] = float(pos_zero.decode())
+                        # orderer_dict[preprocess[0]] = preprocess[1]
+                        # print('newest dict entry', orderer_dict[preprocess[0]])
+                        in_queue.append(each.recv(1500))#[preprocess[0], preprocess[1]]
                     except UnicodeDecodeError:
                         sleep(0.0001)
 
@@ -146,3 +194,6 @@ if __name__ == "__main__":
         print(sys.exc_info())
         sleep(5)
         pass
+    except:
+        print('global error')
+        print(sys.exc_info())
